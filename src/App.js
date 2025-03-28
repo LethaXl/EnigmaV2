b8df7d6b346da31a8b5b9135c2670bd6ba62f3aa
LethaXl <aseef217@gmail.com>
2025-03-28 07:44:42 -0400

    Copy Alert on desktop

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ function App() {
     const [key, setKey] = useState('');
     const [output, setOutput] = useState('');
     const [showOutput, setShowOutput] = useState(false);
+    const [showCopyAlert, setShowCopyAlert] = useState(false);
 
     // New scramble animation effect for the title
     useEffect(() => {
@@ -70,6 +71,10 @@ function App() {
     const handleCopy = async () => {
         try {
             await navigator.clipboard.writeText(output);
+            setShowCopyAlert(true);
+            setTimeout(() => {
+                setShowCopyAlert(false);
+            }, 2000);
         } catch (error) {
             console.error("Copy failed", error);
         }
@@ -225,6 +230,15 @@ function App() {
                             <span className="material-icons-sharp">clear</span>
                         </button>
                     </div>
+                </div>
+            </div>
+            {/* Copy Success Alert */}
+            <div className={`copy-alert ${showCopyAlert ? 'show' : ''}`} role="alert">
+                <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                    <path d="M10 .5a9.5 9.5 0 1 0 9.5 9.5A9.51 9.51 0 0 0 10 .5ZM9.5 4a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM12 15H8a1 1 0 0 1 0-2h1v-3H8a1 1 0 0 1 0-2h2a1 1 0 0 1 1 1v4h1a1 1 0 0 1 0 2Z"/>
+                </svg>
+                <div>
+                    <span className="font-medium">Copied!</span>
                 </div>
             </div>
             <SpeedInsights />
